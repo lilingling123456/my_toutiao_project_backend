e2e87453d98154904546a89be3af152e5af6cce3
zhangsan <zhangsan@sohu.com>
2020-11-25 09:56:57 +0800

    model add email

--- models.py
+++ models.py
@@ -13,6 +13,7 @@ class User(Document):
     photo = StringField(required=True)
     gender = IntField(required=True)
     intro = StringField(required=True)
+    email = StringField(required=True,unique=True)
 
     def to_public_json(self):
         data = {
@@ -21,7 +22,8 @@ class User(Document):
             "created": self.created.strftime("%Y-%m-%d %H:%M:%S"),
             "photo": self.photo,
             "gender": self.gender,
-            "intro":self.intro
+            "intro":self.intro,
+            "email":self.email
         }
 
         return data
--- views/admin.py
+++ views/admin.py
@@ -9,15 +9,16 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import app
 @app.route("/mp/v1_0/authorizations", methods=["POST"])
 def login():
-    # hashed_password = generate_password_hash('246810')
-    # User(
-    #     mobile='13711111111',
-    #     code = hashed_password,
-    #     photo = 'https://image.so.com/view?q=%E5%8F%AE%E5%BD%93%E7%8C%AB%E5%9B%BE%E7%89%87&listsrc=sobox&listsign=26fc7d8ebfd21d493a300af9a7a1ea17&src=360pic_strong&correct=%E5%8F%AE%E5%BD%93%E7%8C%AB%E5%9B%BE%E7%89%87&ancestor=list&cmsid=abe9f60fb0e83117f8c7dfa3b8db68b8&cmras=6&cn=0&gn=0&kn=50&crn=0&bxn=20&fsn=130&cuben=0&pornn=0&manun=0&adstar=0&clw=233#id=fa72ec92e710b9f388321c0cf16573a8&currsn=0&ps=93&pc=93',
-    #     gender = 1,
-    #     name = 'zhangsanfeng',
-    #     intro = 'zhangsanfeng',
-    # ).save()
+    hashed_password = generate_password_hash('246810')
+    User(
+        mobile='13711111111',
+        code = hashed_password,
+        photo = 'https://image.so.com/view?q=%E5%8F%AE%E5%BD%93%E7%8C%AB%E5%9B%BE%E7%89%87&listsrc=sobox&listsign=26fc7d8ebfd21d493a300af9a7a1ea17&src=360pic_strong&correct=%E5%8F%AE%E5%BD%93%E7%8C%AB%E5%9B%BE%E7%89%87&ancestor=list&cmsid=abe9f60fb0e83117f8c7dfa3b8db68b8&cmras=6&cn=0&gn=0&kn=50&crn=0&bxn=20&fsn=130&cuben=0&pornn=0&manun=0&adstar=0&clw=233#id=fa72ec92e710b9f388321c0cf16573a8&currsn=0&ps=93&pc=93',
+        gender = 1,
+        name = 'zhangsanfeng',
+        intro = 'zhangsanfeng',
+        email = '2464792390@qq.com'
+    ).save()
     if not request.json.get("mobile"):
         return jsonify({"error": "Username not specified"}), 409
     if not request.json.get("code"):
