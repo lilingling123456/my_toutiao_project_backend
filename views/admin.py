310122f78cb000ae135279d0380daaef1e993069
zhangsan <zhangsan@sohu.com>
2020-11-26 16:02:10 +0800

    add profile api

--- views/admin.py
+++ views/admin.py
@@ -9,16 +9,16 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import app
 @app.route("/mp/v1_0/authorizations", methods=["POST"])
 def login():
-    hashed_password = generate_password_hash('246810')
-    User(
-        mobile='13711111111',
-        code = hashed_password,
-        photo = 'https://image.so.com/view?q=%E5%8F%AE%E5%BD%93%E7%8C%AB%E5%9B%BE%E7%89%87&listsrc=sobox&listsign=26fc7d8ebfd21d493a300af9a7a1ea17&src=360pic_strong&correct=%E5%8F%AE%E5%BD%93%E7%8C%AB%E5%9B%BE%E7%89%87&ancestor=list&cmsid=abe9f60fb0e83117f8c7dfa3b8db68b8&cmras=6&cn=0&gn=0&kn=50&crn=0&bxn=20&fsn=130&cuben=0&pornn=0&manun=0&adstar=0&clw=233#id=fa72ec92e710b9f388321c0cf16573a8&currsn=0&ps=93&pc=93',
-        gender = 1,
-        name = 'zhangsanfeng',
-        intro = 'zhangsanfeng',
-        email = '2464792390@qq.com'
-    ).save()
+    # hashed_password = generate_password_hash('246810')
+    # User(
+    #     mobile='13711111111',
+    #     code = hashed_password,
+    #     photo = 'https://p2.ssl.qhimgs1.com/sdr/400__/t01baa117299ef7aae8.jpg',
+    #     gender = 1,
+    #     name = 'zhangsanfeng',
+    #     intro = 'zhangsanfeng',
+    #     email = '2464792390@qq.com',
+    # ).save()
     if not request.json.get("mobile"):
         return jsonify({"error": "Username not specified"}), 409
     if not request.json.get("code"):
@@ -41,26 +41,31 @@ def login():
     if not check_password_hash(user.code, request.json.get("code")):
         return jsonify({"error": "Invalid password"}), 401
 
-    # token = jwt.encode({
-    #     "userid": str(user.id),
-    #     "username": user.username,
-    #     "email": user.email,
-    #     "password": user.password,
-    #     "created": str(user.created)
-    # }, app.config["SECRET_KEY"])
+    token = jwt.encode({
+        "name": user.name,
+        "email": user.email,
+        "code": user.code,
+        "created": str(user.created)
+    }, app.config["SECRET_KEY"])
 
     return jsonify({
         # "success": True,
         "message": 'OK',
         "data": {
             "user": user.name,
-            "token": 'xxxxxx', # token.decode("UTF-8")
+            "token": token.decode("UTF-8"), # token.decode("UTF-8")
             # "email": user.email,
             # "password": user.password,
             # "created": str(user.created)
         },
     })
-
+@app.route('/mp/v1_0/user/profile',methods=['GET'])
+def get_user_profile():
+    user = User.objects(name='zhangsanfeng').first()
+    return jsonify({
+        "message": 'OK',
+        "data": user.to_public_json()
+    })
 
 
 
