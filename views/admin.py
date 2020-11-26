27d213504c1514f8d3e741fa40d33a8749ee03c7
zhangsan <zhangsan@sohu.com>
2020-11-26 16:15:36 +0800

    add login_required wrapper

--- views/admin.py
+++ views/admin.py
@@ -5,6 +5,24 @@ from functools import wraps
 from models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if "Authorization" in request.headers:
+            # Check whether token was sent
+            authorization_header = request.headers["Authorization"]
+
+            # Check whether token is valid
+            try:
+                token = authorization_header.split(" ")[1]
+                user = jwt.decode(token, app.config["SECRET_KEY"])
+            except:
+                return jsonify({"error": "you are not logged in"}), 401
+
+            return f(userid=user["userid"], *args, **kwargs)
+        else:
+            return jsonify({"error": "you are not logged in"}), 401
+    return wrap
 
 from app import app
 @app.route("/mp/v1_0/authorizations", methods=["POST"])
@@ -60,8 +78,9 @@ def login():
         },
     })
 @app.route('/mp/v1_0/user/profile',methods=['GET'])
-def get_user_profile():
-    user = User.objects(name='zhangsanfeng').first()
+@login_required
+def get_user_profile(userid):
+    user = User.objects(id=userid).first()
     return jsonify({
         "message": 'OK',
         "data": user.to_public_json()
